abe303ceb4c18298691e1d022062003653d05ef8
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-01-23 08:19:29 +1000

    Refactored Concert and added amount column to it

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -27,15 +27,20 @@ class ConcertOrdersController extends Controller
         ]);
 
         try {
-            $order = $concert->orderTickets(request('email'), request('ticket_quantity'));
+            //Find tickets
+            $tickets = $concert->findTickets(request('ticket_quantity'));
+
+            //Charge the customer for the tickets
             $this->paymentGateway->charge(
                 request('ticket_quantity') * $concert->ticket_price, request('payment_token')
             );
 
+            //Create an order for the tickets
+            $order = $concert->createOrder(request('email'), $tickets);
+
             return response()->json($order->toArray(), 201);
 
         } catch (PaymentFailedException $e) {
-            $order->cancel();
             return response()->json([], 422);
         } catch (NotEnoughTicketsException $e) {
             return response()->json([], 422);

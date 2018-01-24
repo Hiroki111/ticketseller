af0e8e3e65a0877fa8919b101ac610d2ef242c78
Hiroki Takahashi <htakahashi@hyphenio.com.au>
2018-01-25 08:28:11 +1000

    Made Concert belongsToMany Order

--- app/Ticket.php
+++ app/Ticket.php
@@ -16,4 +16,14 @@ class Ticket extends Model
     {
         $this->update(['order_id' => null]);
     }
+
+    public function concert()
+    {
+        return $this->belongsTo(Concert::class);
+    }
+
+    public function getPriceAttribute()
+    {
+        return $this->concert->ticket_price;
+    }
 }
